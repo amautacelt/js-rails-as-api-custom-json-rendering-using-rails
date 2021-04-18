60e88d0153dd3ee52c98688fb07718ad3e42982c
Carlos E. La Torre <amautacelt@gmail.com>
2021-04-17 21:10:35 -0400

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -9,15 +9,16 @@ class BirdsController < ApplicationController
   #   render json: birds, only: [:id, :name, :species]
   # end
 
+
   def index
     birds = Bird.all
     render json: birds, except: [:created_at, :updated_at]
   end
 
-  def index
-    birds = Bird.all
-    render json: birds.to_json(except: [:created_at, :updated_at])
-  end 
+  # def index
+  #   birds = Bird.all
+  #   render json: birds.to_json(except: [:created_at, :updated_at])
+  # end 
 
 
 
@@ -41,6 +42,7 @@ class BirdsController < ApplicationController
   #   render json: {id: bird.id, name: bird.name, species: bird.species } 
   # end 
 
+
   def show
     bird = Bird.find_by(id: params[:id])
     if bird
@@ -49,5 +51,6 @@ class BirdsController < ApplicationController
       render json: { message: 'Bird not found' }
     end
   end 
+
 
 end
